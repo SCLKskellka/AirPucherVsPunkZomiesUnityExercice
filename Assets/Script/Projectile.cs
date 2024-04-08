efa07e5eb3dec7fad9f62ec1a1aa525636a8d05b
popote1 <yvan.bladet@gmail.com>
2024-04-08 08:15:03 +0200

    projectilvisual

--- Assets/Script/Projectile.cs
+++ Assets/Script/Projectile.cs
@@ -8,8 +8,14 @@ public class Projectile : MonoBehaviour {
     public float Speed = 10;
     public int Damage = 1;
     [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Transform _visual;
     void Start() {
         _rigidbody.AddForce(new Vector3(Speed,0,0), ForceMode.Impulse);
+    }
+
+    private void Update() {
+        if (_visual == null) return;
+        _visual.right = -_rigidbody.velocity;
     }
 
     private void OnCollisionEnter(Collision other) {
